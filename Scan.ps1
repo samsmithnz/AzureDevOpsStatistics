ff8cdab5cabe8f047043d10f72d7418c32a3fcd9
Sam Smith <samsmithnz@gmail.com>
2021-09-23 08:13:32 -0400

    Code gardening

--- Scan.ps1
+++ Scan.ps1
@@ -1,27 +1,30 @@
 
 CLS
-$pat = ''
+$pat = '' #Generate a PAT token in Azure DevOps. Select the scope to all organizations if you need to scan multiple organizations
 $InitialOrganizationName = "samsmithnz"
-$organization = $InitialOrganizationName
+$JustScanInitialOrganization = $false
+$getArtifacts = $false
 
-#Initialization
+#Create encrpyted security token
 $base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":$pat"))
 
-# Get Organizations
-$orgRequestBody = "{
-    ""contributionIds"": [""ms.vss-features.my-organizations-data-provider""],
-    ""dataProviderContext"":
-        {
-            ""properties"":{}
-        }
-}"
-#$uri = "https://dev.azure.com/$InitialOrganizationName/_apis/Contribution/HierarchyQuery?api-version=5.0-preview.1"
-#$orgResponse = Invoke-RestMethod -Uri $uri -Body $orgRequestBody -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Post -ErrorAction Stop
-#$orgResponseDetails = $orgResponse.dataProviders
-#$organzationsJson = $orgResponseDetails."ms.vss-features.my-organizations-data-provider".organizations
+# Get all organizations
+if ($JustScanInitialOrganization -eq $true)
+{
+    $orgRequestBody = "{
+        ""contributionIds"": [""ms.vss-features.my-organizations-data-provider""],
+        ""dataProviderContext"":
+            {
+                ""properties"":{}
+            }
+    }"
+    $uri = "https://dev.azure.com/$InitialOrganizationName/_apis/Contribution/HierarchyQuery?api-version=5.0-preview.1"
+    $orgResponse = Invoke-RestMethod -Uri $uri -Body $orgRequestBody -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Post -ErrorAction Stop
+    $orgResponseDetails = $orgResponse.dataProviders
+    $organzations = $orgResponseDetails."ms.vss-features.my-organizations-data-provider".organizations
+}
 
 $summary = @()
-$users = @()
 $artifacts = @()
 $builds = @()
 $releases = @()
@@ -29,78 +32,46 @@ $repos = @()
 $prs = @()
 $files = @()
 $workItems = @()
-#Foreach ($org in $organzationsJson){
-    #This next part is a bit gross, but I can't get PowerShell to convert it into an object. The best I can do is isolate the name property
-    #$organization = ($org | ConvertFrom-String).P2.Replace("name=","").Replace(";","")
-    Write-Host "Processing organization: $organization"
-
-
-
-    ## Get users 
-    ##https://vsaex.dev.azure.com/samsmithnz/_apis/userentitlements?api-version=6.0-preview.3
-    #$uri = "https://vsaex.dev.azure.com/$organization/_apis/userentitlements?api-version=6.0-preview.3&top=1000"
-    #try 
-    #{
-    #    $usersJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
-    #    $today = get-date
-    #    $users += $usersJson.value | Foreach-Object{
-    #        $User = $_.user | ConvertTo-Json | ConvertFrom-Json 
-    #        New-Object -TypeName PSObject -Property @{
-    #            DisplayName = $User.displayName
-    #            MailAddress = $User.mailAddress
-    #            CreatedDate = Get-Date -format "yyyy-MM-dd" $_.dateCreated
-    #            LastAccessedDate = Get-Date -format "yyyy-MM-dd" $_.lastAccessedDate
-    #            DaysSinceLastLogin = $(New-TimeSpan -Start $(Get-Date -format "yyyy-MM-dd" $_.lastAccessedDate) -End $today).Days
-    #            Origin = $User.origin
-    #            AssignementType = $_.accessLevel.assignmentSource
-    #            LicenseDisplayName = $_.accessLevel.licenseDisplayName
-    #            LicensingSource = $_.accessLevel.licensingSource
-    #            Organization = $organization
-    #            ProjectName = $($project.name)
-    #        }
-    #    }
-    #}
-    #catch r
-    #{
-    #    #do nothing 
-    #    Write-Host "No access to $organization users"
-    #}
-
+Foreach ($organization in $organzations){
+    $orgName = $organization.name 
+    Write-Host "Processing organization: $orgName"
 
     # Get Artifacts
-    #https://feeds.dev.azure.com/{organization}/{project}/_apis/packaging/feeds?api-version=6.0-preview.1
-    $uri = "https://feeds.dev.azure.com/$organization/_apis/packaging/feeds?api-version=6.0-preview.1"
-    try 
+    if ($getArtifacts -eq $true)
     {
-        $artifactsJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
-        $artifacts += $artifactsJson | ConvertTo-Json | ConvertFrom-Json | Select-Object -Expand Value | Select name
-    }
-    catch 
-    {
-        #do nothing  
-        Write-Host "No access to $organization artifacts"  
+        #https://feeds.dev.azure.com/{organization}/{project}/_apis/packaging/feeds?api-version=6.0-preview.1
+        $uri = "https://feeds.dev.azure.com/$orgName/_apis/packaging/feeds?api-version=6.0-preview.1"
+        try 
+        {
+            $artifactsJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
+            $artifacts += $artifactsJson | ConvertTo-Json | ConvertFrom-Json | Select-Object -Expand Value | Select name
+        }
+        catch 
+        {
+            #do nothing  
+            Write-Host "No access to $orgName artifacts"  
+        }
     }
 
-
-    # Get projects
+    # Get projects for organization
     #https://dev.azure.com/{organization}/_apis/projects?api-version=5.1
-    $uri = "https://dev.azure.com/$organization/_apis/projects?api-version=5.1"
+    $uri = "https://dev.azure.com/$orgName/_apis/projects?api-version=5.1"
     try
     {
         $projectsJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
-        #$projectsJson | ConvertTo-Json
+        #Extract just the name and last updated date for each project into a new array
         $projects = $projectsJson.value | Foreach-Object{ 
    
             New-Object -TypeName PSObject -Property @{
                 Name = $_.name
                 LastUpdateTime = Get-Date $_.lastUpdateTime
             }
-        } #| Where-Object -Property name -eq "SamLearnsAzure"
+        } 
     }
     catch 
     {
         #do nothing  
-        Write-Host "No access to $organization projects"
+        Write-Host "No access to $orgName projects"
         $projects = @{}  
     }
 
@@ -108,7 +79,7 @@ $workItems = @()
     Foreach ($project in $projects){
 
         # Build runs
-        $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/build/builds?api-version=5.1"
+        $uri = "https://dev.azure.com/$orgName/$($project.name)/_apis/build/builds?api-version=5.1"
         try
         {
             $buildRunsJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
@@ -121,7 +92,7 @@ $workItems = @()
                     Status = $_.status
                     Result = $_.result
                     QueueTime = Get-Date $_.queueTime
-                    Organization = $organization
+                    Organization = $orgName
                     ProjectName = $($project.name)
                 }
             }
@@ -129,17 +100,17 @@ $workItems = @()
         catch 
         {
             #do nothing 
-            Write-Host "No access to $organization $($project.name) builds"   
+            Write-Host "No access to $orgName $($project.name) builds"   
         }   
 
         # Release runs
         # https://vsrm.dev.azure.com/{organization}/{project}/_apis/release/releases?api-version=5.1
-        $uri = "https://vsrm.dev.azure.com/$organization/$($project.name)/_apis/release/releases?api-version=5.1"
+        $uri = "https://vsrm.dev.azure.com/$orgName/$($project.name)/_apis/release/releases?api-version=5.1"
         try
         {
             $releasesJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
             $releases += $releasesJson.value | ForEach-Object {
-                $uri = "https://vsrm.dev.azure.com/$organization/$($project.name)/_apis/release/releases/$($_.id)?api-version=5.1"
+                $uri = "https://vsrm.dev.azure.com/$orgName/$($project.name)/_apis/release/releases/$($_.id)?api-version=5.1"
                 $releaseJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
 
                 $environments = $releaseJson.environments | ConvertTo-Json -Depth 5 | Where-Object status -ne "notStarted" 
@@ -154,7 +125,7 @@ $workItems = @()
                     CreatedOn = if($releaseJson -ne $null) {Get-Date $releaseJson.createdon} else { date }
                     LastEnvironmentName = if($statuses -ne $null) { $statuses[-1].name } else { "none" }
                     LastEnvironmentStatus = if($statuses -ne $null) { $statuses[-1].status } else { "none" } 
-                    Organization = $organization
+                    Organization = $orgName
                     ProjectName = $($project.name)
                 }
             }
@@ -162,12 +133,12 @@ $workItems = @()
         catch 
         {
             #do nothing
-            Write-Host "No access to $organization $($project.name) releases"     
+            Write-Host "No access to $orgName $($project.name) releases"     
         }
 
         # Get work items by project
         $projectWorkItems = @()
-        $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/wit/reporting/workitemrevisions?api-version=5.1&includeDeleted=false"#&includeLatestOnly=true"
+        $uri = "https://dev.azure.com/$orgName/$($project.name)/_apis/wit/reporting/workitemrevisions?api-version=5.1&includeDeleted=false"#&includeLatestOnly=true"
         do {
 
             try
@@ -179,7 +150,7 @@ $workItems = @()
             catch 
             {
                 #do nothing   
-                Write-Host "No access to $organization $($project.name) work items"  
+                Write-Host "No access to $orgName $($project.name) work items"  
             }
 
         } While ($workItemsJson.values.Length -gt 0) #Loop while there are items in the list. Once we reach the end of the list, we will have 0 items    
@@ -189,7 +160,7 @@ $workItems = @()
         #Repos
         #GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=6.0
         $projectRepos = @()
-        $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/git/repositories?api-version=6.0"
+        $uri = "https://dev.azure.com/$orgName/$($project.name)/_apis/git/repositories?api-version=6.0"
         try
         {
             $reposJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
@@ -198,14 +169,14 @@ $workItems = @()
         catch 
         {
             #do nothing   
-            Write-Host "No access to $organization $($project.name) repos"  
+            Write-Host "No access to $orgName $($project.name) repos"  
         }
         $repos += $projectRepos | ConvertTo-Json -Depth 10 | ConvertFrom-Json | Get-Unique -AsString
 
-        #TODO: TFVCs (no PRs)
+        #TFVCs (note there are no PRs - but should we be counting branches?)
         #GET https://dev.azure.com/{organization}/{project}/_apis/tfvc/items?api-version=6.0
         $tfvcRepoExists = $false
-        $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/tfvc/items?api-version=6"
+        $uri = "https://dev.azure.com/$orgName/$($project.name)/_apis/tfvc/items?api-version=6"
         $tfvcJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
         $projectTFVCRepos = $tfvcJson.value | ConvertTo-Json -Depth 10 | ConvertFrom-Json
         if ($projectTFVCRepos.Count -gt 0)
@@ -214,14 +185,14 @@ $workItems = @()
         }
 
         #PRs
-        #Loop through each Repo for PR's
         #GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullrequests?searchCriteria.status=completed&api-version=6.0
+        #Loop through each Repo for PR's
         Foreach ($projectRepo in $projectRepos){
             #if ($projectRepo.name -eq "SamLearnsAzure")
             #{
                 $skipPRs = 0 # The number of pull requests to ignore. For example, to retrieve results 101-150, set top to 50 and skip to 100.
                 $topPRs = 100 # The number of pull requests to retrieve.
-                $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/git/repositories/$($projectRepo.id)/pullrequests?searchCriteria.status=completed&`$skip=$skipPRs&`$top=$topPRs&api-version=6.0"
+                $uri = "https://dev.azure.com/$orgName/$($project.name)/_apis/git/repositories/$($projectRepo.id)/pullrequests?searchCriteria.status=completed&`$skip=$skipPRs&`$top=$topPRs&api-version=6.0"
                 $projectReposPRs = @()
                 $tmp = @()
                 do {
@@ -230,61 +201,48 @@ $workItems = @()
                     $tmp = $projectRepoPRsJson | ConvertTo-Json -Depth 10 | ConvertFrom-Json | Get-Unique -AsString
                     $projectReposPRs += $tmp
                     $skipPRs += $topPRs
-                    $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/git/repositories/$($projectRepo.id)/pullrequests?searchCriteria.status=all&`$skip=$skipPRs&`$top=$topPRs&api-version=6.0"
-                    #Write-Host "$($projectRepo.name) - $skipPRs = $($tmp.Count) , $uri"
+                    $uri = "https://dev.azure.com/$orgName/$($project.name)/_apis/git/repositories/$($projectRepo.id)/pullrequests?searchCriteria.status=all&`$skip=$skipPRs&`$top=$topPRs&api-version=6.0"
                 } While ($tmp.Count -ge 100) #Loop while there are items in the list. Once we reach the end of the list, we will have 0 items    
                 $prs += $projectReposPRs
             #}
 
             $summary += (New-Object -TypeName PSObject -Property @{
-                    Organization = $organization
+                    Organization = $orgName
                     Project = $project.name
                     WorkItemCount = $projectWorkItems.values.Count
                     GitRepo = $projectRepo.name
                     TVFCRepoExists = $tfvcRepoExists
-                    RepoCompressedSizeInMB = "{0:n2}" -f [math]::Round(($projectRepo.size / 1000000),2) # dividing by a million, not exact - but close enough
+                    GitRepoCompressedSizeInMB = "{0:n2}" -f [math]::Round(($projectRepo.size / 1000000),2) # dividing by a million, not exact - but close enough
                     PRsCount = $(if($projectReposPRs.Count -eq $null) {1} else {$projectReposPRs.Count})
                     BuildsAndReleasesCount = $builds.Count + $releases.Count
                 })
         }
-
-        #TODO: Files - Currently can not get file sizes without downloading files...
-        #GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/items?api-version=6.0
-        #if ($projectRepo.name -eq "SamLearnsAzure")
-        #{    
-        #    $uri = "https://dev.azure.com/$organization/$($project.name)/_apis/git/repositories/$($projectRepo.id)/items?recursionLevel=full&includeContentMetadata=true&api-version=6.0"
-        #    $filesJson = Invoke-RestMethod -Uri $uri -ContentType application/json -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Get -ErrorAction Stop
-        #    $projectRepoFiles = $filesJson.value | ConvertTo-Json -Depth 10 | ConvertFrom-Json
-        #    $files += $projectRepoFiles | ConvertTo-Json -Depth 10 | ConvertFrom-Json | Get-Unique -AsString 
-        #}
         
         Write-Host "Scanning project $($project.name) ... ($($repos.Length) Git repos, $(if($tfvcRepoExists -eq $true) {1} else {0}) TFVC repos, $($prs.Length) prs, $($files.Length) files, $($builds.Length) builds, $($releases.Length) releases, and $($workItems.Length) work items found so far)"   
-    }
-#}
+    } # end Foreach ($project in $projects){
+} # end Foreach ($org in $organzationsJson){
 
-#results
-#Write-Host "Total users: $($users.Length)" 
-#$users | ft DisplayName, MailAddress, CreatedDate, LastAccessedDate, DaysSinceLastLogin, Origin, AssignementType, LicenseDisplayName, LicensingSource -auto
-
-Write-Host "Total builds: $($builds.Count)" 
+#Write-Host "Total builds: $($builds.Count)" 
 #$builds | Select Name, Status, Result, QueueTime | Group-Object -Property Status, Result | Select Count, Name | ft
 
-Write-Host "Total releases: $($releases.Count)" 
+#Write-Host "Total releases: $($releases.Count)" 
 #$releases | Select LastEnvironmentStatus | Group-Object -Property LastEnvironmentStatus | Select Count, Name | ft
 
 Write-Host "Total work items: $($workItems.Count)" 
 #$workItems.fields | Select System.WorkItemType, System.ChangedDate | Group-Object -Property System.WorkItemType | Select Count, Name | ft
 
-Write-Host "Total artifact feeds: $($artifacts.Count)"
-#$artifacts | Select name
+if ($getArtifacts -eq $true)
+{
+    Write-Host "Total artifact feeds: $($artifacts.Count)"
+    #$artifacts | Select name
+}
 
 Write-Host "Total Git repos: $($repos.Count)" 
 $TotalReposOver2GB = ($repos | Where-Object size -gt $(2 * ([Math]::Pow(1000,3)))) #1000^3 is a billion/or ~1 GB
 Write-Host "Total Git repos over 2GB: $($TotalReposOver2GB.Count)"
 $TotalReposOver2GB | Select name, size | ft
-
 Write-Host "Total PRs: $($prs.Count)" 
 Write-Host "Total Files: $($files.Count)" 
 
 Write-Host "Summary"
-$summary | ft Organization, Project, WorkItemCount, GitRepo, @{n='RepoCompressedSizeInMB';e={$_.RepoCompressedSizeInMB};align='right'}, PRsCount, TVFCRepoExists #, BuildsAndReleasesCount
+$summary | ft Organization, Project, WorkItemCount, GitRepo, @{n='GitRepoCompressedSizeInMB';e={$_.GitRepoCompressedSizeInMB};align='right'}, PRsCount, TVFCRepoExists #, BuildsAndReleasesCount
